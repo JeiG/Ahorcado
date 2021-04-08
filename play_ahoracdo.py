7e4b51b51888b5630849cee8874b5ab9e759a62d
Jeiber Jimenez <jeiber2001@gmail.com>
2021-04-08 15:46:10 -0500

    Version funcional

--- play_ahoracdo.py
+++ play_ahoracdo.py
@@ -4,17 +4,35 @@ import os
 def works():
     with open('./archivos/data.txt','r',encoding='utf-8') as f:
         works = [work.replace('\n','') for work in f]
+
     random_number = random.randint(1,len(works))
     return works[random_number]
 
 
+def verification(letters,work):
+    clave = ''
+    for i in work:
+        if i in letters:
+            clave += i
+        else:
+            clave += '-' 
+    return clave 
+
+
 def run():
     os.system('cls')
-    input('BIENVENIDO A AHORCADO\nPRESIONA ENTER PARA COMENZAR')
+    input('BIENVENIDO A AHORCADO\nPRESIONA ENTER PARA COMENZAR ')
     work = works()
+    letters = []
+
     while True:
-        print(list(enumerate(work)))
-        break
+        letter = input('Enter a letter: ')
+        letters.append(letter)
+        clave = verification(letters,work)
+        print(clave)
+        if clave == work:
+            print('Has adivinado la palabra')
+            break
 
  
 if __name__ == '__main__':
